2162d351ef3d81c451702d01f0dc2b824ffe8306
alejandracarrenop <alcarrenop@unal.edu.co>
2025-02-25 23:12:10 -0500

    Add files via upload

---
+++ Chat/src/main/java/com/mycompany/chat/Chat.java
@@ -0,0 +1,11 @@
+package com.mycompany.chat;
+
+/**
+ * @author Alejandra Carreño P
+ */
+public class Chat {
+    public static void main(String[] args) {
+        Mensajero1 M1 = new Mensajero1();
+        Mensajero2 M2 = new Mensajero2();
+    }
+}
